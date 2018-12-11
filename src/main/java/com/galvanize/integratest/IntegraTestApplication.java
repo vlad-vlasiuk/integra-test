7509e17d538a7904c4e83c897a9f1ddbf37c7179
vlad-vlasiuk <vlad.vlasiuk@yahoo.com>
2018-12-11 08:51:28 -0700

    create spring boot project
    core: DevTools, Lombok
    web: Web
    sql: Jpa, H2

---
+++ src/main/java/com/galvanize/integratest/IntegraTestApplication.java
@@ -0,0 +1,12 @@
+package com.galvanize.integratest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegraTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegraTestApplication.class, args);
+    }
+}
