845f340901f9ecf01617f25fa0e4936c62f58977
vlad-vlasiuk <vlad.vlasiuk@yahoo.com>
2018-12-11 09:46:02 -0700

    add controller test

--- src/main/java/com/galvanize/integratest/PersonController.java
+++ src/main/java/com/galvanize/integratest/PersonController.java
@@ -1,11 +1,15 @@
 package com.galvanize.integratest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PersonController {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(PersonController.class);
 
     private final PersonService personService;
 
@@ -14,8 +18,9 @@ public class PersonController {
         this.personService = personService;
     }
 
-    @GetMapping
+    @GetMapping("/all")
     public Iterable<Person> getAll() {
+        LOGGER.info("return all Person");
         return personService.findAll();
     }
 
