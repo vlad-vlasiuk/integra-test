afd73406b4858bd32909e176a93dcc82ad249afa
vlad-vlasiuk <vlad.vlasiuk@yahoo.com>
2018-12-11 09:44:51 -0700

    add logging

--- src/main/java/com/galvanize/integratest/PersonService.java
+++ src/main/java/com/galvanize/integratest/PersonService.java
@@ -1,11 +1,15 @@
 package com.galvanize.integratest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class PersonService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PersonService.class);
 
     private final PersonRepository repository;
     private final RestTemplate restTemplate;
@@ -17,12 +21,14 @@ public class PersonService {
     }
 
     Iterable<Person> findAll() {
+        LOGGER.info("find all Person in repository and add email from external resource.");
         Iterable<Person> people = repository.findAll();
         people.forEach(this::setEmail);
         return people;
     }
 
     private void setEmail(Person person) {
+        LOGGER.info("get email from ext. res.");
         person.setEmail(restTemplate
                 .getForEntity("https://haskell-zuul.herokuapp.com/amqp.url", String.class).getBody());
     }
